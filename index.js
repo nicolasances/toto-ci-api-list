b1a57ca0894e44534e07348eef528e201badbfb7
nicolas <nicolas.matteazzi@gmail.com>
2019-02-17 16:59:09 +0100

    Using toto-api-controller

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-var express = require('express');
-var Promise = require('promise');
-var bodyParser = require("body-parser");
+var Controller = require('toto-api-controller');
 
 var getAPIList = require('./dlg/GetAPIList');
 
 var apiName = 'api-list';
 
-var app = express();
+var api = new Controller(apiName);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
-  next();
-});
-app.use(bodyParser.json());
-app.use(express.static('/app'));
+api.path('GET', '/apis', getAPIList);
 
-/**
- * Smoke test api
- */
-app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
-
-app.get('/apis', function(req, res) {
-
-  getAPIList.do().then(function(result) {res.status(200).send(result);}, function(result) {res.status(500).send(result)});
-
-});
-
-app.listen(8080, function() {
-  console.log('Toto CI API List Microservice up and running');
-});
+api.listen();
